c44a2c7a454325177c10314b272d589259112c42
Márcio Diniz <marcio.sccp10@gmail.com>
2023-05-12 15:50:33 -0300

    Updates

---
+++ exercicios/src/componentes/React001.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <h1>Teste 001</h1>;
--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>React</h1>, document.querySelector('#root'));
+import React001 from "./componentes/React001";
+
+
+ReactDOM.render(<React001 />, document.querySelector('#root'));
