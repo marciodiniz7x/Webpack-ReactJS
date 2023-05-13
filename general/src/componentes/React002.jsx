77a1014338793467702c28ddea68d43b1ba71dd7
Márcio Diniz <marcio.sccp10@gmail.com>
2023-05-13 18:57:51 -0300

    Update React002.jsx

--- general/src/componentes/React002.jsx
+++ general/src/componentes/React002.jsx
@@ -1,11 +1,35 @@
 // Bom dia, usuário!
 import React from "react";
 
+// Elementos agrupados como fragmento react
+// const bomDia = (props) => {
+//     return <React.Fragment>
+//                 <h1>Bom dia, {props.nome}!</h1>
+//                 <h2>Seja bem-vinde!</h2>
+//            </React.Fragment>
+// }
+
+// export default bomDia;
+
+// Elementos agrupados como Array
+// const bomDia = (props) => {
+//     return [
+//             <h1>Bom dia, {props.nome}!</h1>,
+//             <h2>Seja bem-vinde!</h2>
+//            ]
+                    
+// }
+
+// export default bomDia;
+
+// Elementos agrupados como div
 const bomDia = (props) => {
-    return <React.Fragment>
-                <h1>Bom dia, {props.nome}!</h1>
-                <h2>Seja bem-vinde!</h2>
-            </React.Fragment>
+    
+    return <div>
+            <h1>Bom dia, {props.nome}!</h1>
+            <h2>Seja bem-vinde!</h2>
+           </div>                    
+            
 }
 
 export default bomDia;
