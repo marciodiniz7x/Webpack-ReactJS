e53b86134ee626ad3d5e13cd323cf1511698f23e
Márcio Diniz <marcio.sccp10@gmail.com>
2023-05-15 14:54:52 -0300

    Update React004.jsx

--- general/src/componentes/React004.jsx
+++ general/src/componentes/React004.jsx
@@ -9,6 +9,14 @@ export default class Saudacao extends Component {
         nome: this.props.nome
     }
 
+    constructor(props) {
+
+        super(props);
+        this.setTipo = this.setTipo.bind(this);
+        this.setNome = this.setNome.bind(this);
+
+    }
+
     setTipo(e) {
 
         this.setState({ tipo: e.target.value });
@@ -29,9 +37,9 @@ export default class Saudacao extends Component {
                 <h1>{tipo}, {nome}</h1>
                 <hr />
                 <input type="text" placeholder="Tipo..." 
-                    value={tipo} onChange={e => this.setTipo(e)} />
+                    value={tipo} onChange={this.setTipo} />
                 <input type="text" placeholder="Nome..." 
-                    value={nome} onChange={e => this.setNome(e)}/>
+                    value={nome} onChange={this.setNome}/>
             </div>
         );
 
