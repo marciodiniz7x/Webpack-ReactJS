80adb617ed1f044775233e71797f1367804fa756
Márcio Diniz <marcio.sccp10@gmail.com>
2023-05-15 15:38:05 -0300

    Updates

--- general/src/index.js
+++ general/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Filho from "./componentes/React005";
 import Pai from "./componentes/React006";
 
 ReactDOM.render(
     <div>
         <Pai nome="Paulo" sobrenome="Silva">
+            <Filho nome="Pedro"/>
+            <Filho nome="Paulo" sobrenome="Silva"/>
+            <Filho nome="Carla" sobrenome="Silva"/>
         </Pai>
     </div>
     , document.querySelector('#root'));
