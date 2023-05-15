64a01ebf4439285a115e82220ddceb23fed4f4f0
Márcio Diniz <marcio.sccp10@gmail.com>
2023-05-15 14:23:25 -0300

    Updates

---
+++ general/src/componentes/React004.jsx
@@ -0,0 +1,25 @@
+// Saudação
+import React, {Component} from "react";
+
+export default class Saudacao extends Component {
+
+    setTipo(e) {
+
+        this.props.tipo = e.target.value;
+
+    }
+
+    render() {
+        
+        const  { tipo, nome } = this.props;
+        return (
+            <div>
+                <h1>{tipo} {nome}</h1>
+                <hr />
+                <input type="text" placeholder="Tipo..." value={tipo} onChange={e => this.setTipo(e)} />
+                <input type="text" placeholder="Nome..." value={nome} />
+            </div>
+        )
+
+    }
+}
--- general/src/index.js
+++ general/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Multi, {BoaNoite} from "./componentes/React003";
+import Saudacao from "./componentes/React004";
 
 ReactDOM.render(
     <div>
-        <Multi.BoaTarde nome="Ana" />
-        <BoaNoite nome="Bia"/>
+        <Saudacao tipo="Bom dia, " nome="Márcio" />
     </div>
     , document.querySelector('#root'));
